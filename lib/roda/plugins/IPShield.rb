7fe79ca0de35296b5cab5d0674242316ae1c824b
Abdullah Janjua <Abdullah.Janjua@dvla.gov.uk>
2022-10-19 12:52:24 +0100

    first fun commit

---
+++ lib/roda/plugins/IPShield.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "IPShield/version"
+
+module Roda
+  module Plugins
+    module IPShield
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/roda/plugins/IPShield/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Roda
+  module Plugins
+    module IPShield
+      VERSION = "0.1.0"
+    end
+  end
+end
