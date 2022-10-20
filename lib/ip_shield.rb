69da9c8aeaf214ebbc55eb9992ae318792d2467f
Abdullah Janjua <Abdullah.Janjua@dvla.gov.uk>
2022-10-20 11:12:23 +0100

    new gem

---
+++ lib/ip_shield.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ip_shield/version"
+
+module IpShield
+  class Error < StandardError; end
+  def self.message
+    'hi there'
+  end
+end
---
+++ spec/ip_shield_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe IpShield do
+  it "has a version number" do
+    expect(IpShield::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
