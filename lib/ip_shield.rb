5e16213ab29a0300e175f7e0a185cae43652019b
Abdullah Janjua <Abdullah.Janjua@dvla.gov.uk>
2022-10-24 10:04:03 +0100

    adding an option to remove and add an ip

--- lib/ip_shield.rb
+++ lib/ip_shield.rb
@@ -8,13 +8,14 @@ class Roda
   module RodaPlugins
     module IpShield
       class UnauthorisedIP < RodaError; end
+
       class InvalidIP < RodaError; end
 
       #
       # @param [Object] app App
       # @param [Array] ip_addrs list of aip addresses
       def self.configure(app, *ip_addrs)
-        ip_addrs =  app.opts[:ip_shield] || ip_addrs
+        ip_addrs = app.opts[:ip_shield] || ip_addrs
         raise InvalidIP, 'No IP is provided' if ip_addrs.nil? || ip_addrs.empty?
 
         ip_addrs.each { |ip_addr| IPValidator.instance.add_ip(ip_addr) }
@@ -22,25 +23,41 @@ class Roda
 
       module RequestMethods
         def is_authorised?
-          IPValidator.instance
+          req_ip_addr = ip_addr(self)
+
+          IPValidator
+            .instance
+            .is_authorize_ip?(req_ip_addr)
         end
 
         def must_be_authorised
+          req_ip_addr = ip_addr(self)
 
+          IPValidator
+            .instance
+            .is_authorize_ip?(req_ip_addr)
+            .tap { |auth| raise UnauthorisedIP, 'The request IP is not authorised' unless auth }
         end
 
         def deauthorize_ip
-          raise InvalidIP, 'No IP is not found' if self&.ip.nil?
-          IPValidator.instance.remove_ip(self.ip)
+          IPValidator
+            .instance
+            .remove_ip(ip_addr(self))
         end
 
         def authorize_ip
-          raise InvalidIP, 'No IP is not found' if self&.ip.nil?
-          IPValidator.instance.add_ip(self.ip)
+          IPValidator
+            .instance
+            .add_ip(ip_addr(self))
+        end
+
+        private
+
+        def ip_addr(request)
+          request&.ip.nil? ? (raise InvalidIP, 'No IP is not found') : request.ip
         end
 
       end
-
     end
 
     register_plugin(:ip_shield, IpShield)
--- lib/ip_shield/ip_validator.rb
+++ lib/ip_shield/ip_validator.rb
@@ -1,23 +1,40 @@
 require "ipaddr"
-require 'singleton'
+require "singleton"
 
+#
 class IPValidator
   include Singleton
   attr_accessor :ips
 
   class InvalidIPsBoundary < StandardError; end
+
   class InvalidIP < StandardError; end
 
-  IP_DOES_NOT_EXIST = 'the given IP is not within the authorised list'
-  INVALID_BOUNDARY = 'IP boundary must be an array of 2 elements: high and low'
-  INVALID_IP = 'IP must be a valid IPv4 or IPv6'
+  INVALID_BOUNDARY = "IP boundary must be an array of 2 elements: high and low".freeze
+  IP_DOES_NOT_EXIST = "the given IP is not within the authorised list".freeze
+  INVALID_IP = "IP must be a valid IPv4 or IPv6".freeze
 
-  def initialize
-    @ips = []
-  end
+  def initialize() @ips = []; end
 
+  # Ensure that all IPs are valid. an IP is invalid if its nil, not a string, or not in a IPv4 or
+  # IPv6 format. In case of a boundary-ip, it also must also be an array of 2
+  # elements (ex. [high, low]). if one or more IP in the list is invalid,
+  # it will raise an error. Otherwise, it will return self
+  #
+  # @example
+  #   invalid_ip = ['0.0....']
+  #   invalid_ip.each {|ip| IPValidator.instance.add_ip(ip)}
+  #
+  #   IPValidator.instance.check_ips # will raise InvalidIP error
+  #
+  # @raise
+  #   * InvalidIPsBoundary: if the array has an invalid number of elements.
+  #   * InvalidIP: if the IP address is invalid
+  #
+  # @param [Array] ip_addr_list a list of all IP address
+  # @return [IPValidator] will return self unless one or more IP is invalid
   def check_ips(ip_addr_list = @ips)
-    ip_addr_list && ip_addr_list.each do |ip_addr|
+    ip_addr_list.each do |ip_addr|
       if ip_addr.is_a?(Array)
         ip_addr
           .tap { |ip| raise InvalidIPsBoundary, INVALID_BOUNDARY unless ip.count.eql?(2) }
@@ -27,18 +44,44 @@ class IPValidator
       end
     end
 
-    return_slef
+    return_self
+  end
+
+  # Checks if the IP is authorized. The IP is authorized if any of the following true:
+  # 1. the IP matches on of the IPs in the list +@ips+
+  # 2. the IP is within any IP boundaries in +@ips+
+  # this function will return true if any 1 or 2 is true. Otherwise, it will return false.
+  #
+  # @example
+  #  IPValidator
+  #     .instance
+  #     .is_authorize_ip?('0.0.0.0')
+  #
+  # @param [String] ip_addr the IP address
+  # @return [TrueClass, FalseClass] true if the IP is authorized. Otherwise return false
+  def is_authorize_ip?(ip_addr)
+    ip_range, ip_list = @ips.partition{|all_ips| all_ips.is_a? Array}
+
+    ip_list.filter!{|ip| IPAddr.new(ip).to_i === IPAddr.new(ip_addr).to_i}
+    ip_list.empty? && ip_range.filter! do |range|
+      low = IPAddr.new(range.first)
+      high = IPAddr.new(range.last)
+      current = IPAddr.new(ip_addr)
+
+      (low..high)===current
+    end
+
+    (ip_list.count + ip_range.count).positive?
   end
 
   def add_ip(ip_addr)
     check_ips([ip_addr]) && @ips.push(ip_addr)
-    return_slef
+    return_self
   end
-
 
   def remove_ip(ip_addr)
     raise InvalidIP, IP_DOES_NOT_EXIST if @ips.delete(ip_addr).nil?
-    return_slef
+    return_self
   end
 
   private
@@ -50,6 +93,5 @@ class IPValidator
     true if IPAddr.new(ip_addr) rescue false
   end
 
-  def return_slef() self; end
-
+  def return_self() self; end
 end
